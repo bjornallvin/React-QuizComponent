fa35012faddc0c30f056907b6fcda0ccd5208b60
Björn Allvin <bjorn.allvin@accenture.com>
2020-04-18 16:15:21 +0200

    commit

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
 
-    handleClick(buttonText){
+        this.state = {
+            incorrectAnswer: false,
+        };
+    }
 
+    handleClick(buttonText) {
         if (buttonText == this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler();
+        }else{
+            this.setState({incorrectAnswer:true})
         }
     }
     render() {
@@ -19,14 +27,16 @@ class QuizQuestion extends Component {
                     <ul>
                         {this.props.quiz_question.answer_options.map(
                             (answer_option, index) => (
-                                <QuizQuestionButton key={index}
+                                <QuizQuestionButton
+                                    key={index}
                                     button_text={answer_option}
-                                    clickHandler = {this.handleClick.bind(this)}
+                                    clickHandler={this.handleClick.bind(this)}
                                 />
                             )
                         )}
                     </ul>
                 </section>
+                {this.state.incorrectAnswer?<p className="error">Sorry, that's not right</p>:null}
             </main>
         );
     }
